46fc367cc7f71b0ea9b9cd2f0eab99fe928ea890
vegvarizsuzsi <110730161+vegvarizsuzsi@users.noreply.github.com>
2023-12-05 12:00:19 +0100

    Initial commit

---
+++ src/main/java/com/codecool/techtask/TechtaskApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.techtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/techtask/TechtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.techtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
