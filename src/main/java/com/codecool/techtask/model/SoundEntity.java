7a406d83265ac92a2345e93e54e103413e52be59
vegvarizsuzsi <110730161+vegvarizsuzsi@users.noreply.github.com>
2023-12-06 17:40:17 +0100

    add the SoundEntity class

---
+++ src/main/java/com/codecool/techtask/model/SoundEntity.java
@@ -0,0 +1,43 @@
+package com.codecool.techtask.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+
+
+@Entity
+@Table(name = "sound")
+@Getter
+@Setter
+public class SoundEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer soundId;
+
+    @Column(nullable = false)
+    private String soundName;
+
+    @Lob
+//    @Column(nullable = false)
+    private byte[] soundData;
+
+    @Column(nullable = false)
+    private String soundExtension;
+
+//    @Column(nullable = false)
+    private LocalDateTime soundCreatedDatetime;
+    private LocalDateTime soundUpdatedDatetime;
+
+
+}
+
